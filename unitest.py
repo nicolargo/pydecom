eebb341f4831c7d8af8c5b8e5a7c8fea2a2b6ac8
Nicolas Hennion <nicolas.hennion@thalesaleniaspace.com>
2019-06-18 10:38:59 +0200

    Correct unitest

--- unitest.py
+++ unitest.py
@@ -5,7 +5,7 @@
 #
 
 import unittest
-from pydecom import DecommutationPlan
+import pydecom
 
 
 class TestPydecom(unittest.TestCase):
@@ -13,11 +13,11 @@ class TestPydecom(unittest.TestCase):
 
     def test_000_structure(self):
         print('=' * 80)
-        s1 = DecommutationPlan('TOP', None)
-        s2 = DecommutationPlan('FIRST', s1)
-        s3 = DecommutationPlan('SECOND', s2)
-        s4 = DecommutationPlan('THIRD', s2)
-        s5 = DecommutationPlan('LAST', s1)
+        s1 = pydecom.DecommutationNode('TOP', None)
+        s2 = pydecom.DecommutationNode('FIRST', s1)
+        s3 = pydecom.DecommutationNode('SECOND', s2)
+        s4 = pydecom.DecommutationNode('THIRD', s2)
+        s5 = pydecom.DecommutationNode('LAST', s1)
         print(s1)
 
 
